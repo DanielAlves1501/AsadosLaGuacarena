b62f132a3267447eec0780c1944ce2c750d0e5a4
Daniel Alves <danielgsmax@gmail.com>
2024-04-22 14:33:05 -0400

    new commit

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,36 +4,61 @@ import "./navBar.css";
 import { Link } from "react-router-dom";
 
 const NavBar = ({ showMobileMenu, setShowMobileMenu }) => {
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 470) {
+      setShowMobileMenu(true);
+    } else {
+      setShowMobileMenu(false);
+    }
+  });
   return (
     <nav
       className="navbar"
       style={{
-        transform: `${showMobileMenu ? "translateX(-100%)" : "translateX(0)"}`,
+        transform: `${showMobileMenu ? "translateX(0)" : "translateX(-100%)"}`,
       }}
     >
       <img src={logo} className="navbar__logo" alt="Logo" />
 
       <ul className="navbar-list">
-        <li className="navbar-list__item">
+        <li
+          className="navbar-list__item"
+          onClick={() => setShowMobileMenu(false)}
+        >
           <Link to="/">Parrillas</Link>
         </li>
-        <li className="navbar-list__item">
+        <li
+          className="navbar-list__item"
+          onClick={() => setShowMobileMenu(false)}
+        >
           {" "}
           <Link to="/pollos">Pollos</Link>
         </li>
-        <li className="navbar-list__item">
+        <li
+          className="navbar-list__item"
+          onClick={() => setShowMobileMenu(false)}
+        >
           {" "}
           <Link to="/platosChef">Platos Chef</Link>
         </li>
-        <li className="navbar-list__item">
+        <li
+          className="navbar-list__item"
+          onClick={() => setShowMobileMenu(false)}
+        >
           {" "}
           <Link to="/combos">Combos</Link>
         </li>
-        <li className="navbar-list__item">
+        <li
+          className="navbar-list__item"
+          onClick={() => setShowMobileMenu(false)}
+        >
           {" "}
           <Link to="/postres">Postres</Link>
         </li>
-        <li className="navbar-list__item">
+        <li
+          className="navbar-list__item"
+          onClick={() => setShowMobileMenu(false)}
+        >
           {" "}
           <Link to="/bebidas">Bebidas</Link>
         </li>
