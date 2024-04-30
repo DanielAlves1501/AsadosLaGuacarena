9bf05dca0b1aa99bcd78e611ab654154ce60e01e
Daniel Alves <danielgsmax@gmail.com>
2024-04-30 11:58:17 -0400

    the location section and map has been added

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -86,6 +86,19 @@ const Home = ({ mainDishes }) => {
             </div>
           </div>
         </section>
+        <section className="location">
+          <h2 className="subtitle">UBICACIÓN</h2>
+          <div className="map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3926.4751599568262!2d-67.8827982!3d10.223207899999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e804248685a6983%3A0x5632e3df91375a8!2sRestaurant%20La%20Guacare%C3%B1a!5e0!3m2!1ses!2sve!4v1714492223904!5m2!1ses!2sve"
+              width="600"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </section>
       </main>
 
       <footer></footer>
